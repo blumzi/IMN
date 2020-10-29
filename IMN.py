c025d5347fdb220884fb781f012c0cf67f876435
Arie Blumenzweig <blumzi@013.net>
2020-10-29 19:57:31 +0200

    Fixed whitespaces

--- IMN.py
+++ IMN.py
@@ -19,14 +19,13 @@ def rmsExternal(captured_night_dir, archived_night_dir, config):
 
 	# Compute how long to wait before capture
 	if start_time != True:
-
 		waitingtime = start_time - timenow
 		remaining_seconds = int(waitingtime.total_seconds())
 
-        lock_file = os.path.join(config.data_dir, config.reboot_lock_file)
-        os.system("touch " + lock_file)
+	lock_file = os.path.join(config.data_dir, config.reboot_lock_file)
+	os.system("touch " + lock_file)
 
-        # Run the IMN shell script
+	# Run the IMN shell script
 	script_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "IMN.sh")
 	os.system(script_path + " {:s}".format(captured_night_dir))
 
@@ -35,5 +34,5 @@ def rmsExternal(captured_night_dir, archived_night_dir, config):
 	os.system(script_path + " {:s} {:s} {:s} {:.6f} {:.6f} {:.1f} {:d} {:d} {:d}".format(config.stationID, \
 		captured_night_dir, archived_night_dir, config.latitude, config.longitude, config.elevation, \
 		config.width, config.height, remaining_seconds))
-        
+
 	os.system("/bin/rm -f " + lock_file)
