08731b93abe51e73242be3c04bbb20f83cbc2117
Arie Blumenzweig <blumzi@013.net>
2020-10-29 19:57:44 +0200

    Cosmetics

--- IMN.sh
+++ IMN.sh
@@ -12,8 +12,8 @@ cd ${captured_dir}
        camera_name=$(basename $(pwd))
        camera_name=${camera_name%%_*}
         ftp_server="ftp://ftp.astronomy.org.il"
-   ftp_credentials="camera@astronomy.org.il:M31Andromeda"
       ftp_location=${ftp_server}/IMN/radiants/${camera_name}
+   ftp_credentials="camera@astronomy.org.il:M31Andromeda"
 radiants_text_file=$(find . -name '*_radiants.txt')
 radiants_text_file=${radiants_text_file#./}
 
